9b601ee6cf516a93da718f4cb8c1dc7fd2089422
bryant <bryant@defrag.in>
2013-12-11 21:00:09 +0100

    add a func body.

--- module-test.hs
+++ module-test.hs
@@ -2,6 +2,7 @@ import Control.Monad.Error
 import LLVM.General
 import LLVM.General.AST
 import LLVM.General.AST.Global
+import qualified LLVM.General.AST.Constant as C
 import LLVM.General.Context
 
 z = withContext $ \c -> runErrorT $ withModuleFromAST c m moduleString
@@ -9,7 +10,12 @@ z = withContext $ \c -> runErrorT $ withModuleFromAST c m moduleString
     m = defaultModule { moduleDefinitions = [asdfFunc] }
     asdfFunc = GlobalDefinition functionDefaults {
         returnType = IntegerType 32,
-        name = Name "asdf"
+        name = Name "asdf",
+        basicBlocks = [
+            BasicBlock (UnName 0) [
+                Name "multiplied" := Mul False False (ConstantOperand $ C.Int 32 32) (ConstantOperand $ C.Int 32 56) []
+                ] (Do $ Ret (Just . LocalReference $ Name "multiplied") [])
+            ]
         }
 
 main = do
